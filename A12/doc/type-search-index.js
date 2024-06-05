81f0925c3e93c922a5e282c7ff038607af49138b
josiahbasilio <basiliojosiah@gmail.com>
2024-06-04 20:53:01 -0400

    Javadoc

--- A12/doc/type-search-index.js
+++ A12/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"p":"<Unnamed>","l":"Menu"},{"p":"<Unnamed>","l":"UiTest"},{"l":"所有类和接口","u":"allclasses-index.html"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Menu"},{"p":"<Unnamed>","l":"UiTest"}];updateSearchResults();
