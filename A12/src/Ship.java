975bd0f7df80b144af863d2689f9976769637262
josiahbasilio <107965549+josiahbasilio@users.noreply.github.com>
2024-07-09 16:13:20 -0400

    Update Ship.java

--- A12/src/Ship.java
+++ A12/src/Ship.java
@@ -1,27 +1,47 @@
 import java.awt.Color;
+import javax.swing.ImageIcon;
 
 public class Ship {
     private String name;
     private int size;
     private boolean isSunk;
+    private ImageIcon bowIcon;
+    private ImageIcon midHullIcon;
+    private ImageIcon sternIcon;
 
-    public Ship(String name, int size) {
+    public Ship(String name, int size, ImageIcon bowIcon, ImageIcon midHullIcon, ImageIcon sternIcon) {
         this.name = name;
         this.size = size;
         this.isSunk = false;
+        this.bowIcon = bowIcon;
+        this.midHullIcon = midHullIcon;
+        this.sternIcon = sternIcon;
     }
 
-    public void placeShip(GameBoard gameBoard, int startX, int startY, int endX, int endY) {
-        // Logic for placing the ship on the game board
-        for (int i = startX; i <= endX; i++) {
-            for (int j = startY; j <= endY; j++) {
-                gameBoard.getGridButtons()[i][j].setBackground(Color.GRAY); // Placeholder for ship placement logic
+    public void placeShip(GameBoard gameBoard, int startX, int startY, boolean horizontal) {
+        for (int i = 0; i < size; i++) {
+            if (horizontal) {
+                if (i == 0) {
+                    gameBoard.getGridButtons()[startX][startY + i].setIcon(bowIcon);
+                } else if (i == size - 1) {
+                    gameBoard.getGridButtons()[startX][startY + i].setIcon(sternIcon);
+                } else {
+                    gameBoard.getGridButtons()[startX][startY + i].setIcon(midHullIcon);
+                }
+            } else {
+                if (i == 0) {
+                    gameBoard.getGridButtons()[startX + i][startY].setIcon(bowIcon);
+                } else if (i == size - 1) {
+                    gameBoard.getGridButtons()[startX + i][startY].setIcon(sternIcon);
+                } else {
+                    gameBoard.getGridButtons()[startX + i][startY].setIcon(midHullIcon);
+                }
             }
+            gameBoard.getGridButtons()[startX + (horizontal ? 0 : i)][startY + (horizontal ? i : 0)].setBackground(Color.GRAY);
         }
     }
 
     public boolean checkIfSunk() {
-        // Logic for checking if the ship is sunk
         return isSunk;
     }
 
