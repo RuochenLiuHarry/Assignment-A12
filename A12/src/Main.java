bf3d55cd1b4efd70a75dbca60ccdba09e3862773
josiahbasilio <107965549+josiahbasilio@users.noreply.github.com>
2024-07-11 00:50:37 -0400

    Update Main.java

--- A12/src/Main.java
+++ A12/src/Main.java
@@ -1,15 +1,10 @@
-import view.GameBoardView;
-import controller.GameController;
-import model.Game;
-import model.Player;
+import view.MenuView;
+import controller.MenuController;
 
 public class Main {
     public static void main(String[] args) {
-        Player player1 = new Player("Player 1", false);
-        Player player2 = new Player("Computer", true);
-        Game game = new Game(player1, player2);
-        GameBoardView gameBoardView = new GameBoardView();
-        GameController gameController = new GameController(game, gameBoardView);
-        gameController.startGame();
+        MenuView menuView = new MenuView();
+        MenuController menuController = new MenuController(menuView);
+        menuController.showMenu();
     }
 }
